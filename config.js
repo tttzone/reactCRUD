d6e8ed5f032228232e321aea8aa97b013a734202
dds telkom <ddstelkom.project@gmail.com>
2017-10-22 05:10:25 +0700

    new connection

--- config.js
+++ config.js
@@ -1,6 +1,18 @@
-<<<<<<< HEAD
+ 
+// var mongo = require("mongoose");
+// var db = mongo.connect("mongodb://localhost:27017/account", function(err, response){
+   // if(err){ console.log('Failed to connect to ' + db); }
+   // else{ console.log('Connected to ' + db, ' + ', response); }
+// });
+
+// module.exports =db;
+
+// reactcrud is database name
+// 192.168.1.71:27017 is your mongo server name 
+
 var mongo = require("mongoose");
-var db = mongo.connect("mongodb://localhost:27017/account", function(err, response){
+// var db = mongo.connect("mongodb://localhost:27017/account", function(err, response){ lokal
+var db = mongo.connect("mongodb://tttzone:k4rt45ur4@ds227045.mlab.com:27045/account", function(err, response){
    if(err){ console.log('Failed to connect to ' + db); }
    else{ console.log('Connected to ' + db, ' + ', response); }
 });
@@ -8,16 +20,4 @@ var db = mongo.connect("mongodb://localhost:27017/account", function(err, respon
 module.exports =db;
 
 // reactcrud is database name
-// 192.168.1.71:27017 is your mongo server name
-=======
-var mongo = require("mongoose");
-var db = mongo.connect("mongodb://localhost:27017/account", function(err, response){
-   if(err){ console.log('Failed to connect to ' + db); }
-   else{ console.log('Connected to ' + db, ' + ', response); }
-});
-
-module.exports =db;
-
-// reactcrud is database name
-// 192.168.1.71:27017 is your mongo server name
->>>>>>> 1a262d336f1690c675653a5f7436e239e7510850
+// 192.168.1.71:27017 is your mongo server name S
